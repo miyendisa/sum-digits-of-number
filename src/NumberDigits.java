6af1860557dc2a3f342a404f37aaaa04efd81c96
Aslıhan <aslihan_hasar@hotmail.com>
2022-12-11 01:56:21 +0300

    Delete remainder variable

--- src/NumberDigits.java
+++ src/NumberDigits.java
@@ -3,15 +3,14 @@ import java.util.Scanner;
 public class NumberDigits {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int number, remainder;
+        int number;
         int sum = 0;
 
-        System.out.println("Enter a number: ");
+        System.out.print("Enter a number: ");
         number = scanner.nextInt();
 
         while (number > 0) {
-            remainder = number % 10;
-            sum += remainder;
+            sum += number % 10;
             number /= 10;
         }
         System.out.println("The sum of the digits of the number: " + sum);
