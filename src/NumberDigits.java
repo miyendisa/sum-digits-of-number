763446a8a589f68d6fc8032f4c94e9def79f0846
Aslıhan <aslihan_hasar@hotmail.com>
2022-12-11 01:39:19 +0300

    Add program codes via upload

---
+++ src/NumberDigits.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class NumberDigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number, remainder;
+        int sum = 0;
+
+        System.out.print("Enter a number: ");
+        number = scanner.nextInt();
+
+        while (number > 0) {
+            remainder = number % 10;
+            sum += remainder;
+            number /= 10;
+        }
+        System.out.print("The sum of the digits of the number: " + sum);
+    }
+}
